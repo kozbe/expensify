3fb17a66874d3b896dfd31d057f2ee4bdae5f405
Ulas Kozbe <u.kozbe@gmail.com>
2018-12-30 01:47:34 -0500

    Edit expense on db

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -42,12 +42,15 @@ export const removeExpense = ({ id } = {}) => ({
 });
 
 export const startRemoveExpense = ({ id }) => {
-  return (dispatch) => {
-    return database.ref(`expenses/${id}`).remove().then(() => {
-      dispatch(removeExpense({ id }));
-    });
+  return dispatch => {
+    return database
+      .ref(`expenses/${id}`)
+      .remove()
+      .then(() => {
+        dispatch(removeExpense({ id }));
+      });
   };
-}
+};
 
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
@@ -56,23 +59,37 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
+export const startEditExpense = (id, updates) => {
+  return dispatch => {
+    return database
+      .ref(`expenses/${id}`)
+      .update(updates)
+      .then(() => {
+        dispatch(editExpense(id, updates));
+      });
+  };
+};
+
 // SET_EXPENSES
 export const setExpenses = expenses => ({
-  type: 'SET_EXPENSES',
+  type: "SET_EXPENSES",
   expenses
 });
 
 export const startSetExpenses = () => {
-  return (dispatch) => {
-    return database.ref('expenses').once('value').then((snapshot) => {
-      const expenses = [];
-      snapshot.forEach((childSnapshot) => {
-        expenses.push({
-          id: childSnapshot.key,
-          ...childSnapshot.val()
+  return dispatch => {
+    return database
+      .ref("expenses")
+      .once("value")
+      .then(snapshot => {
+        const expenses = [];
+        snapshot.forEach(childSnapshot => {
+          expenses.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+          });
         });
+        dispatch(setExpenses(expenses));
       });
-      dispatch(setExpenses(expenses));
-    });
-  }
+  };
 };
