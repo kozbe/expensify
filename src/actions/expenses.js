f9d56587697d0cf040b4ac68d4609e6a39ae2468
Ulas Kozbe <u.kozbe@gmail.com>
2018-12-30 01:34:22 -0500

    Removing expense from db

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,4 +1,3 @@
-import uuid from "uuid";
 import database from "../firebase/firebase";
 
 // ADD_EXPENSE
@@ -41,6 +40,14 @@ export const removeExpense = ({ id } = {}) => ({
   type: "REMOVE_EXPENSE",
   id
 });
+
+export const startRemoveExpense = ({ id }) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }));
+    });
+  };
+}
 
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
